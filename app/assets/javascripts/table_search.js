5b22a6515b7d889923b94aafdfadc71f5feac10c
kindred <kindred@aelogica.com>
2020-05-12 21:40:33 +0800

    Fix search to accept any casing

--- app/assets/javascripts/table_search.js
+++ app/assets/javascripts/table_search.js
@@ -5,7 +5,10 @@ $(function() {
     $("table tr").each(function (index) {
       if (!index) return;
       $(this).find("td").each(function () {
-        var id = $ggfound;
+        var id = $(this).text().toLowerCase().trim();
+        var not_found = (id.indexOf(value) == -1);
+        $(this).closest('tr').toggle(!not_found);
+        return not_found;
       });
     });
   });
